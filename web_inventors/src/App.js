14397b84a419b9dc2a18408ca29eec7bb854d08f
Nasar AHamed Sk <nasar.ahamed@c2info.com>
2025-01-29 18:15:34 +0530

    UI updated

--- web_inventors/src/App.js
+++ web_inventors/src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Web Inventors</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="textFont">Web Inventors</p>
       </header>
     </div>
   );
